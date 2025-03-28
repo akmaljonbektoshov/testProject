27bcb79f6bd8f51491cfb4549d29e3491a75ba30
akmaljon <akmaljonbektoshov169@gmail.com>
2025-03-28 16:21:24 +0500

    added User file

--- src/User.java
+++ src/User.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class User {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
